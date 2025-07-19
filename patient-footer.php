196bdeef68b3fa12c031ed55f6204a9a8ae52120
Manvir Singh <147043473+manvirsinghh@users.noreply.github.com>
2025-07-19 04:44:41 -0600

    Delete patient-footer.php

--- patient-footer.php
+++
@@ -1,7 +0,0 @@
-<!-- patient-footer.php -->
-<footer class="bg-dark text-white py-3 fixed-bottom">
-  <div class="container text-center">
-    <p class="mb-1">Need Help? <a href="contactus.php" class="text-info text-decoration-none">Contact Support</a></p>
-    <p class="mb-0">© 2025 HMS | Logged in as <strong>Patient</strong></p>
-  </div>
-</footer>
